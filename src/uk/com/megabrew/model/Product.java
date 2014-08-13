ee4f4925d81743ed0d9359484403abc1d74d5291
Lucas Churchill <lucas@agtlucas.com>
2014-08-13 09:28:17 -0300

    Update Product.java

--- src/uk/com/megabrew/model/Product.java
+++ src/uk/com/megabrew/model/Product.java
@@ -1,5 +1,75 @@
 package uk.com.megabrew.model;
 
 public class Product {
+	
+	private String name;
+	private String description;
+	private String unitMeasure;
+	private String volume;
+	private int amount;
+	private Double price;
+	
+	public Product() {
+		super();
+	}
+	
+	public Product(String name, String description, String unitMeasure, String volume, int amount, Double price) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.unitMeasure = unitMeasure;
+		this.volume = volume;
+		this.amount = amount;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUnitMeasure() {
+		return unitMeasure;
+	}
+
+	public void setUnitMeasure(String unitMeasure) {
+		this.unitMeasure = unitMeasure;
+	}
+
+	public String getVolume() {
+		return volume;
+	}
+
+	public void setVolume(String volume) {
+		this.volume = volume;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
 
 }
