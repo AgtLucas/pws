83c4acd638951f724cd8fe7d70777287fa30d1a4
Lucas Churchill <lucas@agtlucas.com>
2014-08-13 10:09:17 -0300

    Update service

---
+++ src/uk/com/megabrew/service/ListBeers.java
@@ -0,0 +1,24 @@
+package uk.com.megabrew.service;
+
+import java.util.List;
+
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+import uk.com.megabrew.dao.ProductDAO;
+import uk.com.megabrew.model.Product;
+
+@WebService
+public class ListBeers {
+
+	public List<Product> listBeers() {
+		ProductDAO productDAO = new ProductDAO();
+		return productDAO.getBeers();
+	}
+	
+	public static void main(String args[]) {
+		Endpoint.publish("http://localhost:8180/Products", new ListBeers());
+		System.out.println("Yeah, you know, localhost:8180/Products, k?");
+	}
+	
+}
