37db7a3b32d9d020fc8e34f71707491f3f8b8f66
Lucas Churchill <lucas@agtlucas.com>
2014-08-25 08:33:36 -0300

    Update CustomerDAO

--- src/uk/com/megabrew/dao/CustomerDAO.java
+++ src/uk/com/megabrew/dao/CustomerDAO.java
@@ -1,5 +1,35 @@
 package uk.com.megabrew.dao;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import uk.com.megabrew.model.Customer;
+import uk.com.megabrew.service.UserNotAuthorizedException;
+
 public class CustomerDAO {
+	
+	public static List<Customer> customers = new ArrayList<Customer>();
+	
+	static {
+		Customer customer = new Customer();
+		customer.setId(1);
+		customer.setUsername("lucas@lucas.ninja");
+		customer.setPassword("666TheNumberOfTheBeast");
+		customers.add(customer);
+	}
+	
+	public boolean auth(Customer customer) throws UserNotAuthorizedException {
+		for (Customer c : customers) {
+			if (c.getUsername().equals(customer.getUsername()) && c.getPassword().equals(customer.getPassword())) {
+				return true;
+			}
+		}
+		
+		throw new UserNotAuthorizedException("Unable to authorize your login.");
+	}
+	
+	public Customer searchCustomer(Customer customer) {
+		return customer;
+	}
 
 }
