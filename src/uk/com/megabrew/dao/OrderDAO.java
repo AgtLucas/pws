f719a3c5f3b199065aff5317555c31796c3c14cb
Lucas Churchill <lucas@agtlucas.com>
2014-08-25 08:21:12 -0300

    Update OrderDAO

--- src/uk/com/megabrew/dao/OrderDAO.java
+++ src/uk/com/megabrew/dao/OrderDAO.java
@@ -1,5 +1,20 @@
 package uk.com.megabrew.dao;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import uk.com.megabrew.model.Order;
+
 public class OrderDAO {
+	
+	private static List<Order> o = new ArrayList<Order>();
+	
+	public List<Order> getOrder() {
+		return o;
+	}
+	
+	public void createOrder(Order order) {
+		o.add(order);
+	}
 
 }
