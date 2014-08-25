9557ab4bfaf5150193db798a573cae5e8c65da3f
Lucas Churchill <lucas@agtlucas.com>
2014-08-25 08:09:34 -0300

    Update UserNotAuthorizedException

--- src/uk/com/megabrew/service/UserNotAuthorizedException.java
+++ src/uk/com/megabrew/service/UserNotAuthorizedException.java
@@ -1,5 +1,27 @@
 package uk.com.megabrew.service;
 
-public class UserNotAuthorizedException {
+public class UserNotAuthorizedException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public UserNotAuthorizedException() {
+		super();
+	}
+	
+	public UserNotAuthorizedException(String message, Throwable cause, boolean enableSuppresion, boolean writableStackTrace) {
+		super(message, cause, enableSuppresion, writableStackTrace);
+	}
+	
+	public UserNotAuthorizedException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public UserNotAuthorizedException(String message) {
+		super(message);
+	}
+	
+	public UserNotAuthorizedException(Throwable cause) {
+		super(cause);
+	}
 
 }
