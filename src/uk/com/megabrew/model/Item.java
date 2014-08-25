0d99cba0d4fd98f08db05cdef390eb479f877bf6
Lucas Churchill <lucas@agtlucas.com>
2014-08-25 08:17:37 -0300

    Update item model

--- src/uk/com/megabrew/model/Item.java
+++ src/uk/com/megabrew/model/Item.java
@@ -1,5 +1,65 @@
 package uk.com.megabrew.model;
 
 public class Item {
+	
+	private long id;
+	private Order order;
+	private Product product;
+	private String name;
+	private double totalValue;
+	private int amount;
+
+	public Item() {
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(double totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
 
 }
