c214b81b06cc0f9aa32d74725178f0982e1b3198
Lucas Churchill <lucas@agtlucas.com>
2014-09-03 14:10:11 -0300

    Update Customer

--- src/uk/com/megabrew/model/Customer.java
+++ src/uk/com/megabrew/model/Customer.java
@@ -2,10 +2,7 @@ package uk.com.megabrew.model;
 
 public class Customer {
 	
-	private long id;
 	private String name;
-	private String username;
-	private String password;
 	private int phone;
 	
 	public Customer() {
@@ -14,19 +11,8 @@ public class Customer {
 	
 	public Customer(long id, String name, String username, String password, int phone) {
 		super();
-		this.id = id;
 		this.name = name;
-		this.username = username;
-		this.password = password;
 		this.phone = phone;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
 	}
 
 	public String getName() {
@@ -35,22 +21,6 @@ public class Customer {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public int getPhone() {
