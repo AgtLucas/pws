4c98825f996dc239c98869bb5940308c50530f1d
Lucas Churchill <lucas@agtlucas.com>
2014-09-03 13:40:13 -0300

    Update CustomerNormal

--- src/uk/com/megabrew/model/CustomerNormal.java
+++ src/uk/com/megabrew/model/CustomerNormal.java
@@ -1,5 +1,27 @@
 package uk.com.megabrew.model;
 
-public class CustomerNormal {
+public class CustomerNormal extends Customer {
+	
+	private String cpf;
+
+	public CustomerNormal() {
+		super();
+	}
+
+	public CustomerNormal(long id, String name, String username,
+			String password, int phone) {
+		super(id, name, username, password, phone);
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
 
 }
