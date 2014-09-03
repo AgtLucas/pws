c4ae5cc38248f22b11dadbefcde6a1fe0e57dff1
Lucas Churchill <lucas@agtlucas.com>
2014-09-03 14:12:16 -0300

    Update User

---
+++ src/uk/com/megabrew/model/User.java
@@ -0,0 +1,42 @@
+package uk.com.megabrew.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class User {
+	
+	private String name;
+	private String username;
+	private String password;
+	
+	public User() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
