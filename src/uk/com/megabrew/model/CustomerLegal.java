9fd86794db2bd0bdae898c1b32fd13b02715d841
Lucas Churchill <lucas@agtlucas.com>
2014-09-03 14:08:05 -0300

    Update CustomerLegal

--- src/uk/com/megabrew/model/CustomerLegal.java
+++ src/uk/com/megabrew/model/CustomerLegal.java
@@ -1,5 +1,47 @@
 package uk.com.megabrew.model;
 
-public class CustomerLegal {
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+public class CustomerLegal extends Customer {
+	
+	private String cnpj;
+	
+	@XmlEnum
+	@XmlType(name = "locationtype")
+	public enum locationType {
+		
+		@XmlEnumValue("bar")
+		BAR,
+		
+		@XmlEnumValue("pub")
+		PUB,
+		
+		@XmlEnumValue("restaurant")
+		RESTAURANT,
+		
+		@XmlEnumValue("cafeteria")
+		CAFETERIA,
+		
+	}
+
+	public CustomerLegal() {
+		super();
+	}
+
+	public CustomerLegal(long id, String name, String username,
+			String password, int phone) {
+		super(id, name, username, password, phone);
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
 
 }
