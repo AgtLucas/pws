3f576f2281036df688152cfef02fea077e05b0a6
Lucas Churchill <lucas@agtlucas.com>
2014-09-03 23:50:56 -0300

    Update UserNotAuthorizedException

--- src/uk/com/megabrew/service/UserNotAuthorizedException.java
+++ src/uk/com/megabrew/service/UserNotAuthorizedException.java
@@ -1,5 +1,17 @@
 package uk.com.megabrew.service;
 
+import java.util.GregorianCalendar;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeConstants;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.ws.WebFault;
+
+@WebFault(targetNamespace = "http://service.megabrew.com.uk/exceptions", name="UserNotAuthorized")
 public class UserNotAuthorizedException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
@@ -22,6 +34,35 @@ public class UserNotAuthorizedException extends Exception {
 	
 	public UserNotAuthorizedException(Throwable cause) {
 		super(cause);
+	}
+	
+	public UserFaultInfo getFaultInfo() {
+		return new UserFaultInfo(getMessage());
+	}
+	
+	@XmlAccessorType(XmlAccessType.FIELD)
+	public static class UserFaultInfo {
+		
+		@XmlAttribute
+		private String message;
+		private XMLGregorianCalendar date;
+		
+		public UserFaultInfo(String message) {
+			this.message = message;
+			
+			try {
+				this.date = DatatypeFactory.newInstance().newXMLGregorianCalendar(new GregorianCalendar());
+				this.date.setMillisecond(DatatypeConstants.FIELD_UNDEFINED);
+				this.date.setTimezone(DatatypeConstants.FIELD_UNDEFINED);
+			} catch (DatatypeConfigurationException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+		public UserFaultInfo() {
+			
+		}
+		
 	}
 
 }
