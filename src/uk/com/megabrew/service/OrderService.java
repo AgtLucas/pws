7259929366f2e5956cd99e4ccb7afa6be533aa81
Lucas Churchill <lucas@agtlucas.com>
2014-09-03 23:57:24 -0300

    Update OrderService...

--- src/uk/com/megabrew/service/OrderService.java
+++ src/uk/com/megabrew/service/OrderService.java
@@ -5,12 +5,21 @@ import java.util.List;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPConstants;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPFactory;
+import javax.xml.soap.SOAPFault;
 import javax.xml.ws.Endpoint;
+import javax.xml.ws.soap.SOAPFaultException;
 
-import uk.com.megabrew.model.*;
 import uk.com.megabrew.dao.CustomerDAO;
 import uk.com.megabrew.dao.OrderDAO;
 import uk.com.megabrew.dao.ProductDAO;
+import uk.com.megabrew.model.Customer;
+import uk.com.megabrew.model.Item;
+import uk.com.megabrew.model.Order;
+import uk.com.megabrew.model.Product;
 
 @WebService
 public class OrderService {
@@ -26,13 +35,20 @@ public class OrderService {
 	
 	@WebResult(name = "pedido")
 	public List<Order> createOrder(@WebParam(name = "pedido") Order order, @WebParam(name = "usuario", header = true) Customer customer)
-		throws UserNotAuthorizedException {
+		throws UserNotAuthorizedException, SOAPException {
 		
 		if (customerDAO.auth(customer)) {
 			processOrder(order, customer);
 			orderDAO.createOrder(order);
 			return orderDAO.getOrder();
+		} else if (customer.getName().equals("faultCode")) {
+			SOAPFault soapFault = SOAPFactory.newInstance().createFault("Yo dawg, you're not authorized!",
+					new QName(SOAPConstants.URI_NS_SOAP_1_1_ENVELOPE, "Client.authorized"));
+			soapFault.setFaultActor("http://service.megabrew.com.uk/OrderService");
+			
+			throw new SOAPFaultException(soapFault);
 		}
+		
 		return null;
 	}
 	
