2123a4a608356fb4aba48d8dabe1d208e419c21b
Lucas Churchill <lucas@agtlucas.com>
2014-09-03 23:33:42 -0300

    Adding DateAdapter

---
+++ src/uk/com/megabrew/model/DateAdapter.java
@@ -0,0 +1,34 @@
+package uk.com.megabrew.model;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.datatype.DatatypeConstants;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+public class DateAdapter extends XmlAdapter<XMLGregorianCalendar, Date> {
+	
+	@Override
+	public XMLGregorianCalendar marshal(Date date) throws Exception {
+		
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(date);
+		XMLGregorianCalendar xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
+		xmlGregorianCalendar.setMillisecond(DatatypeConstants.FIELD_UNDEFINED);
+		xmlGregorianCalendar.setTimezone(DatatypeConstants.FIELD_UNDEFINED);
+		
+		return xmlGregorianCalendar;
+		
+	}
+
+	@Override
+	public Date unmarshal(XMLGregorianCalendar v) throws Exception {
+		
+		Date date = v.toGregorianCalendar().getTime();
+		return date;
+		
+	}
+
+}
